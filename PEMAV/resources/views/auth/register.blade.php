cb6dff7f1d764d0c336ef621cf4bd91b5f7f4675
Francisco Joshua Quintero Montero <143279487+joshuaqm@users.noreply.github.com>
2024-01-07 18:11:39 -0600

    [COMMIT] Registro
    Adapté la vista de registro, ya está conectado a la base de datos

--- PEMAV/resources/views/auth/register.blade.php
+++ PEMAV/resources/views/auth/register.blade.php
@@ -1,52 +1,115 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!doctype html>
+<html lang="es">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<head>
+  <title>Registrarse en PEMAV</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <link href="{{ asset('assets/estilos.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="header gradient-custom-2">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-auto">
+                    <img src="{{ asset('images/PEMAV_logo.png') }}" alt="Descripción de la imagen" class="logo-header">
+                </div>
+                <div class="col-lg">
+                    <h3 class="header-text">PEMAV</h3>
+                </div>
+            </div>
         </div>
+    </div>
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10">
+                    <div class="card rounded-3 text-black">
+                        <div class="row g-0">
+                            <div class="col-lg-6">
+                                <div class="card-body p-md-5 mx-md-4">
+                                    <div class="text-center">
+                                        <h4 class="mt-1 mb-5 pb-1 text-primary">Registrar nuevo usuario</h4>
+                                    </div>
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                    <!-- Name -->
+                                    <div>
+                                        <x-input-label for="name" :value="__('Nombre completo:')" />
+                                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                    </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <!-- Email Address -->
+                                    <div class="mt-4">
+                                        <x-input-label for="email" :value="__('Correo electrónico:')" />
+                                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    </div>
+
+                                    <!-- Password -->
+                                    <div class="mt-4">
+                                        <x-input-label for="password" :value="__('Contraseña:')" />
+
+                                        <x-text-input id="password" class="block mt-1 w-full"
+                                                        type="password"
+                                                        name="password"
+                                                        required autocomplete="new-password" />
+
+                                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                    </div>
+
+                                    <!-- Confirm Password -->
+                                    <div class="mt-4">
+                                        <x-input-label for="password_confirmation" :value="__('Confirmar contraseña:')" />
+
+                                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                                        type="password"
+                                                        name="password_confirmation" required autocomplete="new-password" />
+
+                                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                    </div>
+
+                                    <div class="flex items-center justify-end mt-4 text-center">
+                                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                                            {{ __('¿Ya estás registrado?') }}
+                                        </a>
+
+                                        <x-primary-button class="btn gradient-custom-2 ms-4">
+                                            {{ __('Registro') }}
+                                        </x-primary-button>
+                                    </div>
+                                    </form>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 d-flex align-items-center">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h1 class="mb-4 text-primary">Registro en PEMAV.</h1>
+                                    <h4 class="mb-2 text-dark">Calidad y experiencia.</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+      <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+</html>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
