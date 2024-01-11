b8c2dd93b65d0d20d492c0735fda283c44d660eb
Francisco Joshua Quintero Montero <143279487+joshuaqm@users.noreply.github.com>
2024-01-10 19:58:37 -0600

    [COMMIT] Vista de calificaciones
    El alumno ya puede ver sus calificaciones por materia

--- PEMAV/app/Http/Controllers/CalificacionesController.php
+++ PEMAV/app/Http/Controllers/CalificacionesController.php
@@ -3,20 +3,76 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Calificaciones;
 
 class CalificacionesController extends Controller
 {
     public function index()
     {
-        // Aquí podrías obtener las calificaciones desde tu base de datos
-        //$calificaciones = [
-            //['materia' => 'Matemáticas', 'calificacion' => 90],
-            //['materia' => 'Ciencias', 'calificacion' => 85],
-            //['materia' => 'Historia', 'calificacion' => 95],
-            // ... más calificaciones
-        //];
+        $alumno = Auth::user()->name;
+        $id_alumno = Auth::user()->id;
+
+        $promedioEspañol = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 1)->avg('calificacion');
+        $promedioMatematicas = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 2)->avg('calificacion');
+        $promedioHabilidadMatematica = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 3)->avg('calificacion');
+        $promedioHabilidadVerbal = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 4)->avg('calificacion');
+        $promedioFisica = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 5)->avg('calificacion');
+        $promedioQuimica = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 6)->avg('calificacion');
+        $promedioBiologia = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 7)->avg('calificacion');
+        $promedioHistoria = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 8)->avg('calificacion');
+        $promedioGeografia = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 9)->avg('calificacion');
+        $promedioFormacionCivica = Calificaciones::where('id_alumno', $id_alumno)->where('id_asignatura', 10)->avg('calificacion');
         
-        //return view('calificaciones.index', compact('calificaciones'));
-        return view('calificaciones');
+        $contadorMaterias = 0;
+        $promedioTotal = 0;
+        if($promedioEspañol != null){
+            $promedioTotal += $promedioEspañol;
+            $contadorMaterias++;
+        }
+        if($promedioMatematicas != null){
+            $promedioTotal += $promedioMatematicas;
+            $contadorMaterias++;
+        }
+        if($promedioHabilidadMatematica != null){
+            $promedioTotal += $promedioHabilidadMatematica;
+            $contadorMaterias++;
+        }
+        if($promedioHabilidadVerbal != null){
+            $promedioTotal += $promedioHabilidadVerbal;
+            $contadorMaterias++;
+        }
+        if($promedioFisica != null){
+            $promedioTotal += $promedioFisica;
+            $contadorMaterias++;
+        }
+        if($promedioQuimica != null){
+            $promedioTotal += $promedioQuimica;
+            $contadorMaterias++;
+       }
+        if($promedioBiologia != null){
+            $promedioTotal += $promedioBiologia;
+            $contadorMaterias++;
+        }
+        if($promedioHistoria != null){
+            $promedioTotal += $promedioHistoria;
+            $contadorMaterias++;
+        }
+        if($promedioGeografia != null){
+            $promedioTotal += $promedioGeografia;
+            $contadorMaterias++;
+        }
+        if($promedioFormacionCivica != null){
+            $promedioTotal += $promedioFormacionCivica;
+            $contadorMaterias++;
+        }
+        $promedioTotal = $promedioTotal / $contadorMaterias;
+        
+        $calificaciones = Calificaciones::where('id_alumno', $id_alumno)->get();
+
+        return view('calificaciones', compact('alumno', 'calificaciones', 
+        'promedioEspañol', 'promedioMatematicas', 'promedioHabilidadMatematica', 
+        'promedioHabilidadVerbal', 'promedioFisica', 'promedioQuimica', 'promedioBiologia', 
+        'promedioHistoria', 'promedioGeografia', 'promedioFormacionCivica', 'promedioTotal'));
     }
 }
--- PEMAV/resources/views/calificaciones.blade.php
+++ PEMAV/resources/views/calificaciones.blade.php
@@ -15,80 +15,259 @@
 </head>
 
 <body>
-@include('layouts.navigation')
-<section class='container my-5'>
-<h1 class="mb-4">Calificaciones</h1>
-<br>
-    <style>
-        table {
-            width: 80%; /* Ancho de la tabla */
-            margin: auto; /* Centrar la tabla */
-            border-collapse: collapse; /* Colapsar los bordes de la tabla */
-        }
-        th, td {
-            padding: 10px; /* Añadir espaciado interno a las celdas */
-            text-align: center; /* Centrar texto dentro de las celdas */
-            border: 1px solid #000; /* Añadir bordes a las celdas */
-        }
-        th {
-            background-color: #f2f2f2; /* Color de fondo para las celdas de encabezado */
-        }
-    </style>
-    <table>
-    <thead>
-        <tr>
-            <th>Asignatura</th>
-            <th>Primer Periodo</th>
-            <th>Segundo Periodo</th>
-            <th>Tercer Periodo</th>
-            <th>Promedio de asignatura</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Asignatura 1</td>
-            <td>Calificacion Asignatura 1</td>
-            <td>Calificacion Asignatura 1</td>
-            <td>Calificacion Asignatura 1</td>
-            <td>Promedio Asignatura 1</td>
-        </tr>
-        <tr>
-            <td>Asignatura 2</td>
-            <td>Calificacion Asignatura 2</td>
-            <td>Calificacion Asignatura 2</td>
-            <td>Calificacion Asignatura 2</td>
-            <td>Calificacion Asignatura 2</td>
-        </tr>
-        <tr>
-            <td>Asignatura 3</td>
-            <td>Calificacion Asignatura 3</td>
-            <td>Calificacion Asignatura 3</td>
-            <td>Calificacion Asignatura 3</td>
-            <td>Promedio Asignatura 3</td>
-        </tr>
-        <tr>
-            <td>Asignatura 4</td>
-            <td>Calificacion Asignatura 4</td>
-            <td>Calificacion Asignatura 4</td>
-            <td>Calificacion Asignatura 4</td>
-            <td>Promedio Asignatura 4</td>
-        </tr>
-        <tr>
-            <td>Promedio del periodo</td>
-            <td>Primer Periodo</td>
-            <td>Segundo Periodo</td>
-            <td>Tercer Periodo</td>
-            <td>Promedio Final</td>
-        </tr>
-    </tbody>
-</table>
+  @include('layouts.navigation')
+  <section class='container my-5'>
+    <h1 class="mb-4">Calificaciones por asignatura</h1>
+    <div class='text-center'>
+      <h3>Español</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 1) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 1) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioEspañol }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
 
-</section>
+    <div class='text-center'>
+      <h3>Matemáticas</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 2) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 2) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioMatematicas }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Habilidad Matemática</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 3) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 3) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Habilidad Verbal</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 4) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 4) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Física</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 5) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 5) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Química</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 6) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 3) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Biología</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 7) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 7) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Historia</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 8) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 8) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Geografía</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 9) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 9) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+    <div class='text-center'>
+      <h3>Formación Cívica y Ética</h3>
+      <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                @foreach($calificaciones->where('id_asignatura', 10) as $calificacion)
+                  <th>Examen: {{ $calificacion->numero_examen }}</th>
+                @endforeach
+                <th>Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>{{ $alumno }}</td>
+              @foreach($calificaciones->where('id_asignatura', 10) as $calificacion)
+                <td>{{ $calificacion->calificacion }}</td>
+              @endforeach
+              <td>{{ $promedioHabilidadMatematica }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div><br><br>
+
+  </section>
 
 <br>
 <div class='text-center'>
-    <h3>Promedio Final:</h3>
-</div>
+    <h3>Promedio Total: {{ $promedioTotal }}</h3>
+</div><br><br>
+
+  <footer class="footer gradient-custom-2" style="padding: 50px">
+
+  </footer>
 
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
