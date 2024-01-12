09e8b24082d9cae3f24a334ac1aaf39d08580c76
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2024-01-11 19:37:07 -0600

    ultimos cambios
    Corregido la navbar de ver grupos y quitamos cosas del login

--- PEMAV/resources/views/layouts/navigation.blade.php
+++ PEMAV/resources/views/layouts/navigation.blade.php
@@ -33,7 +33,7 @@
                                 <a class="nav-link nav-option" href="{{ route('crear-usuarios')}}">Ver/Registrar/Dar de baja usuarios</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link nav-option" href="{{ route('calificaciones')}}">Ver estadísticas</a>
+                                <a class="nav-link nav-option" href="{{ route('calificaciones')}}">Ver grupos</a>
                             </li>
                             @else
                             <li class="nav-item">
@@ -43,7 +43,7 @@
                                 <a class="nav-link nav-option" href="{{ route('modificar-calificaciones')}}">Modificar calificaciones</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link nav-option" href="{{ route('estadisticas.index')}}">Ver estadísticas</a>
+                                <a class="nav-link nav-option" href="{{ route('estadisticas.index')}}">Ver Grupos</a>
                             </li>
                             @endif
                         </ul>
